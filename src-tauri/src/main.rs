c50bd7a3143a0c10b0141068978b65051098ed75
zhilong <zhouzhilong@nullht.cn>
2023-02-09 14:28:40 +0800

    init

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,146 @@
+#![cfg_attr(
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
+)]
+
+use std::{collections::HashMap, fs::File};
+use std::io::Read;
+
+use serde::Serialize;
+use zip::ZipArchive;
+
+#[derive(Debug)]
+struct Path {
+    parts: Vec<String>,
+}
+
+impl Path {
+    pub fn new(path: &str) -> Path {
+        Path {
+            parts: path.to_string().split("/").map(|s| s.to_string()).collect(),
+        }
+    }
+}
+
+// A recursive type to represent a directory tree.
+// Simplification: If it has children, it is considered
+// a directory, else considered a file.
+#[derive(Debug, Clone, Serialize)]
+struct Dir {
+    title: String,
+    key: String,
+    children: Vec<Dir>,
+    selectable: bool,
+}
+
+impl Dir {
+    fn new(name: &str, key: &str) -> Dir {
+        Dir {
+            title: name.to_string(),
+            key: key.to_string(),
+            children: Vec::<Dir>::new(),
+            selectable: false,
+        }
+    }
+
+    fn find_child(&mut self, name: &str) -> Option<&mut Dir> {
+        for c in self.children.iter_mut() {
+            if &c.title == name {
+                return Some(c);
+            }
+        }
+        None
+    }
+
+    fn add_child<T>(&mut self, leaf: T) -> &mut Self
+        where
+            T: Into<Dir>,
+    {
+        self.children.push(leaf.into());
+        self
+    }
+}
+
+fn dir(val: &str, key: &str) -> Dir {
+    Dir::new(val, key)
+}
+
+pub static mut CURRENT_ZIP: Option<ZipArchive<File>> = None;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> Vec<Dir> {
+    println!("greet execute");
+    let zip_file = open(name);
+    let file_names: Vec<Path> = zip_file
+        .file_names()
+        .map(|x| Path::new(x))
+        .collect::<Vec<Path>>();
+    let mut top = dir("root", "0");
+
+    for (index, path) in file_names.iter().enumerate() {
+        build_tree(&mut top, &path.parts, 0);
+    }
+    unsafe {
+        CURRENT_ZIP = Some(zip_file);
+    }
+    top.children
+}
+
+#[tauri::command]
+fn read_xml(key: &str) -> String {
+    unsafe {
+        let zip = CURRENT_ZIP.as_mut().unwrap();
+        let mut file = zip.by_name(key).unwrap();
+        let mut xml = String::new();
+        file.read_to_string(&mut xml);
+        xml
+    }
+}
+
+
+fn build_tree(node: &mut Dir, parts: &Vec<String>, depth: usize) {
+    if depth < parts.len() {
+
+        let item = &parts[depth];
+
+        let mut dir = match node.find_child(&item) {
+            Some(d) => d,
+            None => {
+                let key = parts[..=depth].join("/");
+                let d = Dir::new(&item, &key);
+                node.add_child(d);
+                match node.find_child(&item) {
+                    Some(d2) => d2,
+                    None => panic!("Got here!"),
+                }
+            }
+        };
+        build_tree(&mut dir, parts, depth + 1);
+    }else{
+        if node.title.ends_with(".xml") || node.title.ends_with(".rels"){
+            node.selectable = true;
+        }
+    }
+}
+
+
+#[derive(Serialize)]
+struct TreeNode {
+    title: String,
+    key: String,
+    children: Vec<TreeNode>,
+}
+
+pub fn open(path: &str) -> ZipArchive<File> {
+    let file: File = File::open(path).unwrap();
+    let zip: ZipArchive<File> = ZipArchive::new(file).unwrap();
+    return zip;
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet,read_xml])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/App.tsx
@@ -0,0 +1,187 @@
+import {useEffect, useState} from "react";
+import reactLogo from "./assets/react.svg";
+import {invoke} from "@tauri-apps/api/tauri";
+import "./App.css";
+import 'antd/dist/reset.css'
+import Editor, {DiffEditor, useMonaco, loader} from "@monaco-editor/react";
+import type {DataNode} from 'antd/es/tree'
+import {Button, Layout, Tree} from "antd";
+import {Content, Footer, Header} from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import {listen} from "@tauri-apps/api/event";
+
+const containerStyle: React.CSSProperties = {
+    height: window.innerHeight
+}
+
+const headerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#565656',
+    height: 32,
+    paddingInline: 50,
+    lineHeight: '32px',
+    backgroundColor: '#F2F2F2',
+    borderBottom: "solid 1px",
+    borderTop: "solid 1px",
+    borderColor: "#D0D0D0"
+};
+const siderStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#050505',
+    height: 'inherit',
+    overflow: 'scroll',
+    backgroundColor: '#FFFFFF',
+    borderRight: "solid 1px",
+    borderColor: "#D0D0D0"
+};
+
+const contentStyle: React.CSSProperties = {
+    minHeight: 120,
+    height: 'inherit',
+    overflow: 'scroll',
+    whiteSpace: "pre",
+    color: "#000000"
+
+};
+
+
+const initTreeData: DataNode[] = [];
+
+
+function App() {
+    const [treeData, setTreeData] = useState(initTreeData);
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [filePath, setFilePath] = useState("");
+
+    useEffect(() => {
+        const unlisten = listen("tauri://file-drop", (event) => {
+            let file_path: any = event.payload;
+            setTitle(file_path);
+            (async () => {
+                console.log(file_path);
+                let name = file_path.toString();
+                let nodes: DataNode[] = await invoke("greet", {name});
+                console.log("nodes", nodes);
+                setTreeData(nodes);
+                setContent("");
+            })();
+        });
+        return () => {
+            unlisten.then(f => f());
+        }
+    }, []);
+
+
+    document.onkeydown = (e: KeyboardEvent) => {
+        if (e.code == "KeyF" && e.metaKey) {
+            console.log("press ctrl+f");
+        }
+    }
+
+    const onSelect = (selectedKeys: React.Key[], info: any) => {
+        console.log(selectedKeys);
+        let key = selectedKeys[0];
+
+        (async () => {
+            // setFilePath(key.toString());
+            let content: string = await invoke("read_xml", {key});
+            let pretty_xml = formatXml(content);
+            console.log(pretty_xml);
+            setContent(pretty_xml);
+
+        })();
+
+        console.log('selected', selectedKeys, info);
+    };
+
+    //计算头函数 用来缩进
+    const setPrefix = (prefixIndex: any) => {
+        var result = '';
+        var span = '    ';//缩进长度
+        var output = [];
+        for (var i = 0; i < prefixIndex; ++i) {
+            output.push(span);
+        }
+        result = output.join('');
+        return result;
+    }
+    //格式化xml代码
+    const formatXml = (xmlStr: any) => {
+        let text = xmlStr;
+        //使用replace去空格
+        text = '\n' + text.replace(/(<\w+)(\s.*?>)/g, function ($0: any, name: any, props: any) {
+            return name + ' ' + props.replace(/\s+(\w+=)/g, " $1");
+        }).replace(/>\s*?</g, ">\n<");
+        console.log("1", text);
+        //处理注释，对注释进行编码
+        text = text.replace(/\n/g, '\r').replace(/<!--(.+?)-->/g, function ($0: any, text: any) {
+            var ret = '<!--' + escape(text) + '-->';
+            return ret;
+        }).replace(/\r/g, '\n');
+        console.log("2", text);
+        //调整格式  以压栈方式递归调整缩进
+        var rgx = /\n(<(([^\?]).+?)(?:\s|\s*?>|\s*?(\/)>)(?:.*?(?:(?:(\/)>)|(?:<(\/)\2>)))?)/mg;
+        var nodeStack: any = [];
+        var output = text.replace(rgx, function ($0: any, all: any, name: any, isBegin: any, isCloseFull1: any, isCloseFull2: any, isFull1: any, isFull2: any) {
+            var isClosed = (isCloseFull1 == '/') || (isCloseFull2 == '/') || (isFull1 == '/') || (isFull2 == '/');
+            var prefix = '';
+            if (isBegin == '!') {//!开头
+                prefix = setPrefix(nodeStack.length);
+            } else {
+                if (isBegin != '/') {///开头
+                    prefix = setPrefix(nodeStack.length);
+                    if (!isClosed) {//非关闭标签
+                        nodeStack.push(name);
+                    }
+                } else {
+                    nodeStack.pop();//弹栈
+                    prefix = setPrefix(nodeStack.length);
+                }
+            }
+            var ret = '\n' + prefix + all;
+            return ret;
+        });
+        console.log("output", output);
+        var prefixSpace = -1;
+        var outputText = output.substring(1);
+        //还原注释内容
+        outputText = outputText.replace(/\n/g, '\r').replace(/(\s*)<!--(.+?)-->/g, function ($0: any, prefix: any, text: any) {
+            if (prefix.charAt(0) == '\r')
+                prefix = prefix.substring(1);
+            // 解码
+            text = unescape(text).replace(/\r/g, '\n');
+            var ret = '\n' + prefix + '<!--' + text.replace(/^\s*/mg, prefix) + '-->';
+            return ret;
+        });
+        outputText = outputText.replace(/\s+$/g, '').replace(/\r/g, '\r\n');
+        return outputText;
+    }
+
+
+    return (
+
+        <Layout style={containerStyle}>
+            <Header style={headerStyle}>{title}</Header>
+            <Layout>
+                <Sider style={siderStyle}>
+                    <Tree onSelect={onSelect} treeData={treeData}>
+
+                    </Tree>
+
+                </Sider>
+                <Content style={contentStyle}>
+                    <Editor
+
+                        // height="90vh"
+                        path={filePath}
+                        defaultLanguage="xml"
+                        value={content}
+                    />
+                </Content>
+            </Layout>
+        </Layout>
+    );
+}
+
+export default App;
